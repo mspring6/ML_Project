3bc8e6d6a4077eacf6a86127c5d6fbb47dc74ccf
Matthew <springsteenmatthew@gmail.com>
2023-04-11 10:26:58 -0600

    script to build the maze

---
+++ main.py
@@ -0,0 +1,43 @@
+import logging
+from enum import Enum, auto
+
+import matplotlib.pyplot as plt
+import numpy as np 
+
+#import models
+from Environment.maze import Maze, Render
+
+logging.basicConfig(format="%(levelname)-8s: %(asctime)s: %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    level=logging.INFO)  # Only show messages *equal to or above* this level
+
+class Test(Enum):
+    SHOW_MAZE_ONLY = auto()
+    DEEP_Q = auto()
+
+test = Test.SHOW_MAZE_ONLY #select which test to run
+
+maze = np.array([
+    [0,0,0,0,0],
+    [1,0,0,0,1],
+    [1,0,0,1,1],
+    [1,0,0,0,1],
+    [1,1,0,0,1],
+    [1,0,0,0,1],
+    [1,0,0,0,1],
+    [1,0,0,1,1],
+    [1,1,0,0,1],
+    [1,0,0,1,1],
+    [1,0,0,0,1],
+    [0,0,0,0,0]
+    ])
+
+game = Maze(maze)
+
+#only show the maze
+if test == Test.SHOW_MAZE_ONLY:
+    game.render(Render.MOVES)
+    game.reset()
+
+
+plt.show()
